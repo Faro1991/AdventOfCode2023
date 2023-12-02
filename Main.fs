22e43c55a4fca265e9e033e071728fb5d518f4f9
Fnord <19fnord@gmail.com>
2023-12-02 20:41:50 +0100

    rebuild for benchmarking

--- Main.fs
+++ Main.fs
@@ -2,21 +2,13 @@
 open AoC.Day1
 open System.Text.RegularExpressions
 
-module main =
+[<EntryPoint>]
+let main argv =
 
     let day1Input = System.IO.File.ReadAllText("Day1/Day1Input.txt")
 
     let day1part1 = day1Input |> Day1.partOne
     printfn "%d" day1part1
-
-    let example = """two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-1fiveight78fiveight"""
 
     let day1part2 = day1Input |> Day1.partTwo
     printfn "%d" day1part2
